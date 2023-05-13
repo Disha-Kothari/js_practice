e16d849656e6f73e64705a121c29125d4eafe52c
disha-kothari <disha.kothari@improwised.com>
2023-05-13 17:36:54 +0530

    Execercise1-fetching Git Users

--- gitUsers.js
+++ gitUsers.js
@@ -4,32 +4,64 @@
 //token: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
 
 url="https://api.github.com/users"
-const users=["disha-kothari","Payal17122000","test"]
-const TOKEN="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"
-let temp;
-let jobs = [];
- console.time("start")
-users.forEach(element => {
-   fetch(url+"/"+element, {
+const users=["disha-kothari","Payal17122000","disha_kothari"];
+const TOKEN="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";
+console.time("timer");
+let jobs=[];
+
+let methodType=1;
+if (methodType===1){//promise
+    users.forEach(element => {
+    let job =fetch(url+"/"+element, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${TOKEN}`
+            }})
+        .then((response)=>{
+            if(response.status!=200){
+                return null;
+            }
+            else{
+                return response.json();
+            }
+        })
+        .catch((error)=>{return error})
+        jobs.push(job)
+    });
+
+    Promise.allSettled([...jobs]).then((results)=>{
+        for (let i=0;i<results.length;i++){
+            console.log(results)
+        }
+        console.timeEnd("timer");   
+    })
+}
+else if (methodType==2)//async await
+{
+    getUsers(users)
+}
+async function getUsers(names) {
+    console.time("timer2") 
+    for(let name of names) {
+      let job = await fetch(`${url}/${name}`, {
         method: "GET",
         headers: {
-            Authorization: `Bearer ${TOKEN}`,
-        }})
-    .then((response)=>{
-        if(response.status!=200){
-            return null;
-        }
-        else{
-            return response.json();
-        }
-    })
-    .then(data=>{jobs.push(data)})
-    .catch((error)=>{return error})
-});
-
-console.log(jobs)
-//temp+=jobs
-console.timeEnd("start")
-
-document.getElementById("data").innerHTML=jobs.length;
-    
+            Authorization: `Bearer ${TOKEN}`
+        }}).then(
+        (successResponse) => {
+            if (successResponse.status!=200){
+                return null
+            }
+            else{
+                return successResponse.json();
+            }
+          },
+          (error)=>{
+            return error
+          }
+        )
+        .then((data)=>console.log(data))
+       }
+    console.timeEnd("timer2")
+   
+}
